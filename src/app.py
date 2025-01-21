d2c3fc9d922b8dbd58dc95b5be1b4fb002c883cd
Jamylle-7 <jamylle.fernandes@academico.ifpb.edu.br>
2025-01-21 17:40:05 -0300

    será que agora vai

--- src/app.py
+++ src/app.py
@@ -1,11 +1,5 @@
-from flask import Flask
+from waitress import serve
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-ggi
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=8080)
