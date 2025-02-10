0a76718e56a80e07b0f7c4bf2b0b1497f99a107a
Jamylle-7 <jamylle.fernandes@academico.ifpb.edu.br>
2025-02-10 15:30:24 -0300

    criei branch

---
+++ static/js/atalhoParaTopo.js
@@ -0,0 +1,43 @@
+alert('testsetest)
+
+
+
+window.addEventListener('load', function(){
+    var atalhoTopo = document.querySelector('.atalho-topo')
+    var topo
+
+    window.onscroll = function(){
+        topo = window.scrollY
+        if (topo >= 400){
+            atalhoTopo.style.display = "block"
+        }else{
+            atalhoTopo.style.display = "none"
+        }
+    }
+
+    atalhoTopo.onclick = function(){
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+})window.addEventListener('load', function(){
+    var atalhoTopo = document.querySelector('.atalho-topo')
+    var topo
+
+    window.onscroll = function(){
+        topo = window.scrollY
+        if (topo >= 400){
+            atalhoTopo.style.display = "block"
+        }else{
+            atalhoTopo.style.display = "none"
+        }
+  }
+
+    atalhoTopo.onclick = function(){
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+}
---
+++ static/js/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mensagem = document.getElementById('mensagem').value;
+    if (mensagem) {
+        alert(mensagem);
+    }
+});
